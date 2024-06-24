7f4ad343c46bf89085bb0ca98cc779b65ae537ff
SF-testUmnyasha <newashedelo@gmail.com>
2024-06-24 16:36:31 +0300

    Initial Commit

---
+++ King Buffalo's Realm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  King Buffalo's Realm
+//
+//  Created by Oleg Arnaut  on 24.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ King Buffalo's Realm/King_Buffalo_s_RealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  King_Buffalo_s_RealmApp.swift
+//  King Buffalo's Realm
+//
+//  Created by Oleg Arnaut  on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct King_Buffalo_s_RealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
